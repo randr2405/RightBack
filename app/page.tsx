ca4d8653493cf26a2d4bc344c81ca0c0560cd77c
Rhea Jugernath <randr2405@gmail.com>
2026-09-23 13:10:47 +0200

    home pg x13

--- app/page.tsx
+++ app/page.tsx
@@ -2947,7 +2947,7 @@ export default function Home() {
             scrollEase={0.05}
             fontUrl=""
             font="bold 30px Orbitron"
-            scrollSpeed={0.05}
+            scrollSpeed={0.01}
           />
         </div>
       </section>
